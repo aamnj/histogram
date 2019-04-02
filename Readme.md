39e48fe4c793c608e82439354bcc3f39f3e6b979
Aman jain <aman.jain@raweng.com>
2019-04-02 11:56:24 +0530

    histogram

---
+++ Readme.md
@@ -0,0 +1,6 @@
+Histogram
+
+Get Started
+1. Clone repo and run command npm install or npm i
+2. run npm start
+3. test.txt contains test inputs
---
+++ histogram.js
@@ -0,0 +1,46 @@
+const fs = require("fs")
+var data = fs.readFileSync("./test.txt").toString("utf-8")
+var dataInString = data.split("\n")[0]
+
+var arr        = dataInString.split(",")
+var bucketSize = data.split("\n")[1]
+
+/**
+ * returns array of elements in each bucket starting from beginning
+ * @param {*} arr arr of numbers to create histogram
+ * @param {*} bucketSize size of bucket
+ */
+function histogram(arr, bucketSize) {
+  var maxInArray   = Math.max.apply(Math, arr);
+  var totalBuckets = Math.round(maxInArray / bucketSize) + 1
+  var result       = []
+
+  for (var i = 0; i < totalBuckets; i++) {
+    var maxInBucket = bucketSize*(i+1)
+    var minInBucket = bucketSize*i
+    
+    var tempArray   = arr.filter(function(elem) {
+      return((elem > minInBucket) && (elem <= maxInBucket))
+    })
+
+    result.push(tempArray.length)
+  }
+  return result
+}
+
+/**
+ * function to display histogram
+ * @param {arr} arr 
+ * 
+ */
+function showHistogram(arr) {
+  arr.forEach(elem => {
+    var str = ""
+    while (elem--) {
+      str = str.concat("#")
+    }
+    console.log(str)
+  })
+}
+
+showHistogram(histogram(arr, bucketSize))
